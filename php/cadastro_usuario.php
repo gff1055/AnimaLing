b9386a1011e729b684d3f550a5f62d32c1f6c8e9
gff1055 <gff105@gmail.com>
2017-06-26 17:18:21 -0300

    Adicao de scripts e jquery. implementacao da pagina de cadastro

--- php/cadastro_usuario.php
+++ php/cadastro_usuario.php
@@ -4,24 +4,41 @@ $testRegister=new DonoConBD();
 
 <html>
 	<head>
+		<link rel="stylesheet" href="/js/jquery-ui.css" />
+		<script src="/js/jquery-1.8.2.js"></script>
+		<script src="/js/jquery-ui.js"></script>
 	</head>
 	<body>
-		<form action="testebusca.php" method="GET">
-			<input type="text" name="nome" />
-			<!--input type="text" name="sobrenome" />
-			<input type="text" name="usuario" /-->
+		<form action="cadastro_usuario.php" method="POST">
+			<label for="firstName">Nome:</label><input type="text" name="firstName" id="firstName"/><br>
+			<label for="lastName">Sobrenome:</label><input type="text" name="lastName" id="lastName"/><br>
+			<label for="user">Usuario:</label><input type="text" name="user" id="user"/><br>
+			<label for="password">Senha:</label><input type="text" name="password" id="password"/><br>
+			<label for="birthday">Nascimento:</label>
+			<input type="text" name="birthday" id="birthday"/><br>
+			<label for="email">Email:</label><input type="text" name="email" id="email"/><br><br>
 			<input type="submit" />
 		</form>
 	</body>
 	<?php
-	$testFeedBack = $testRegister->checkEmail("b@eu.com.br");
-		if($testFeedBack == DonoConBD::EMAIL_EXISTS)
-			echo "<br>EMAIL EXISTE<br>";
-		elseif($testFeedBack == DonoConBD::EMAIL_INVALID)
-			echo "<br>O EMAIL INSERIDO ESTA INVALIDO<br>";
-		elseif($testFeedBack == DonoConBD::EMAIL_ALLOWED)
-			echo "<br>ESTE EMAIL PODE SER CADASTRADO<br>";
-		else
-			echo "<br>ERRO DESCONHECIDO<br>";
+	if(!empty($_POST)){
+		
+		switch($testFeedBack){
+			
+			case DonoConBD::EMAIL_EXISTS:
+				echo "<br>EMAIL EXISTE<br>";
+				break;
+			case DonoConBD::EMAIL_INVALID:
+				echo "<br>O EMAIL INSERIDO ESTA INVALIDO<br>";
+				break;
+			case DonoConBD::EMAIL_ALLOWED:
+				echo "<br>ESTE EMAIL PODE SER CADASTRADO<br>";
+				break;
+			default:
+				echo "<br>ERRO DESCONHECIDO<br>";
+				break;
+				
+		}
+	}
 	?>
 </html>
